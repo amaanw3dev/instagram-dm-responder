2137406b7cc628aae15f31f8622436e54c041c9f
amaan35 <amaanlm35@gmail.com>
2024-05-15 16:47:24 +0530

    correcting the path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const port = process.env.PORT;
 
-app.get("/verify-webhook", (req, res) => {
+app.get("/webhook", (req, res) => {
   
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
