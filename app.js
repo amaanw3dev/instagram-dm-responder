9afad53940a8e7efc814737b6fa94b51fa3abaa7
amaan35 <amaanlm35@gmail.com>
2024-05-15 16:27:24 +0530

    verifying webhook using the verify token and responding with the challenge received in the request

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const express = require("express")
+const express = require("express");
 const dotenv = require("dotenv");
 
-const app = express()
+const app = express();
 dotenv.config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-const response = process.env.RESPONSE;
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.get("/", (req, res)=>{
-  res.send(response)
-})
+app.get("/verify-webhook", (req, res) => {
+  console.log("request : ", req);
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+  console.log("the three params : ", mode, token, challenge);
+
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
+function handleMessage(sender_psid, received_message) {}
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
+function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
+function callSendAPI(sender_psid, response) {}
 
-app.listen(port, ()=>console.log("server started at port ", port))
+app.listen(port, () => console.log("server started at port ", port));
